68926232deb8c1c749f16ac4c3153bceeeb131c0
Retardicus <aivoton.idiootti@gmail.com>
2019-05-06 13:28:29 +0300

    check if element on screen

--- js/mainScripts.js
+++ js/mainScripts.js
@@ -88,4 +88,57 @@ function saatiedot(lat, lon) {
   }
 }
 
+/*
+ * End of weather&background functions
+ */
 
+/*
+ * Functions for checking if element on screen
+ */
+// on screen checker
+const gambitGalleryIsInView = el => {
+  const scroll = window.scrollY || window.pageYOffset
+  const boundsTop = el.getBoundingClientRect().top + scroll
+
+  const viewport = {
+    top: scroll,
+    bottom: scroll + window.innerHeight,
+  }
+
+  const bounds = {
+    top: boundsTop,
+    bottom: boundsTop + el.clientHeight,
+  }
+
+  return ( bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom )
+      || ( bounds.top <= viewport.bottom && bounds.top >= viewport.top );
+}
+
+
+// Usage.
+document.addEventListener( 'DOMContentLoaded', () => {
+  const tester = document.querySelector( '#test' )
+  const answer = document.querySelector( '#answer' )
+
+  const handler = () => raf( () => {
+    if (gambitGalleryIsInView( tester )) {
+      alert("TEST");
+    }
+  } )
+
+  handler()
+  window.addEventListener( 'scroll', handler )
+} )
+
+// requestAnimationFrame
+const raf =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function( callback ) {
+      window.setTimeout( callback, 1000 / 60 )
+    }
+
+/*
+ * End of functions for checking if element is on screen
+ */
